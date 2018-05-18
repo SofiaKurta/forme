28596f38fa2fd2136f088adc8a0bb827b2dd3877
Cinex <cinex@i.ua>
2018-05-18 09:36:50 +0300

    fix open_faucet

--- Lessons/Different/open_faucet.sh
+++ Lessons/Different/open_faucet.sh
@@ -9,3 +9,4 @@ url7="https://freenem.com/free"
 url8=""
 google-chrome -new-window $url -new-tab $url2 -new-tab $url3 -new-tab $url4 -new-tab $url5 -new-tab $url6 -new-tab $url7 &
 
+#chmod u+x open_faucet.sh
